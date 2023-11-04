d96c6109fef4950f27d040a5c5d9630c2df24b7f
Lineked <lineked@yandex.ru>
2023-11-04 11:55:05 +0300

    minor changes

--- src/main/kotlin/com/ktor/notes/data/users/Users.kt
+++ src/main/kotlin/com/ktor/notes/data/users/Users.kt
@@ -13,5 +13,5 @@ object Users: Table("users") {
     val login = Users.varchar("login", 25)
     val password = Users.varchar("password", 25)
     val username = Users.varchar("username", 30)
-    val email = Users.varchar("login", 30)
+    val email = Users.varchar("email", 30)
 }
