332293405a5d850d0926924f4f1dd28ec3a3d027
Lineked <lineked@yandex.ru>
2023-11-07 21:18:10 +0300

    Added new routes

--- src/main/kotlin/com/ktor/notes/plugins/Routing.kt
+++ src/main/kotlin/com/ktor/notes/plugins/Routing.kt
@@ -1,23 +1,23 @@
 package com.ktor.notes.plugins
 
 
+import com.ktor.notes.data.notes.NotesDaoImpl
 import com.ktor.notes.data.users.UsersDaoImpl
-import com.ktor.notes.features.login.loginRouting
-import com.ktor.notes.features.notes.notesRouting
-import com.ktor.notes.features.register.registerRouting
+import com.ktor.notes.routes.login.loginRouting
+import com.ktor.notes.routes.notes.notesRouting
+import com.ktor.notes.routes.register.registerRouting
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.resources.Resources
 import io.ktor.server.routing.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(usersDao: UsersDaoImpl, notesDao: NotesDaoImpl) {
     install(Resources)
     routing {
-        val usersDao = UsersDaoImpl()
         loginRouting(usersDao)
         registerRouting(usersDao)
         authenticate("auth-jwt") {
-            notesRouting()
+            notesRouting(notesDao)
         }
 
     }
